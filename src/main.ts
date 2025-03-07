14ff923262acf7aa79b3bdb31ab9c8533a6c10c0
ankitasood <asood@octolabs.io>
2025-03-07 10:48:34 -0600

    generated form

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { importProvidersFrom } from '@angular/core';
 import { PetListComponent } from './app/components/pet-list/pet-list.component';
 import { PetDetailComponent } from './app/components/pet-detail/pet-detail.component';
 import {App} from "./app/app/app.component";
-import {PetFormComponent} from "./app/components/pet-form/pet-form.component";
+import {PetAdoptFormComponent} from "./app/components/pet-adopt-form/pet-adopt-form.component";
 
 bootstrapApplication(App, {
   providers: [
     provideRouter([
       { path: '', component: PetListComponent },
       { path: 'pet/:id', component: PetDetailComponent },
-      { path: 'adopt/:id', component: PetFormComponent }
+      { path: 'adopt/:id', component: PetAdoptFormComponent }
     ]),
     importProvidersFrom(BrowserAnimationsModule)
   ]
