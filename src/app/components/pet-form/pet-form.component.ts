0344d1ae477ea2a8916ef90c550c43400dafef6c
ankitasood <asood@octolabs.io>
2025-03-07 10:05:12 -0600

    adoption form component

---
+++ src/app/components/pet-form/pet-form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  template: `
+    <p>
+      pet-form works!
+    </p>
+  `,
+  styles: ``
+})
+export class PetFormComponent {
+
+}
